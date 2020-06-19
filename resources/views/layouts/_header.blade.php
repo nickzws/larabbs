cfc7cf2824cf7e87a5213e484cb2432a7a1d87d0
winne <554808642@qq.com>
2020-06-19 11:47:59 +0800

    认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,10 +22,8 @@
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
 {{--        @guest--}}
-          <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-{{--          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>--}}
-{{--          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>--}}
-          <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
 {{--        @else--}}
 {{--          <li class="nav-item">--}}
 {{--            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">--}}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', 'PagesController@root')->name('root');
+
+//Auth::routes();
+
+
+// 用户身份验证相关的路由
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 用户注册相关路由
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// 密码重置相关路由
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
